ac7a6aed05b636c295aa2fad354d85344e9ecdcc
mikramarc <michal@shadowrobot.com>
2018-01-04 17:38:36 +0000

    positional control working

--- sr_mechanism_controllers/src/srh_grasp_controller.cpp
+++ sr_mechanism_controllers/src/srh_grasp_controller.cpp
@@ -179,6 +179,8 @@ namespace controller
 
   void SrhGraspController::update(const ros::Time &time, const ros::Duration &period)
   {
+    int lol = 0;
+    
     if (!initialized_)
     {
       resetJointState();
@@ -188,12 +190,16 @@ namespace controller
     double error_position;
     double commanded_effort;
     bool in_deadband;
-    
+   
     for (int i = 0; i < joints_.size(); ++i)
     {
       if (2 == joints_[i].size())
       {
           has_j2_=true;
+      }
+      else
+      {
+          has_j2_=false;
       }
         
       if (!has_j2_ && !joints_[i][0]->calibrated_)
@@ -203,13 +209,13 @@ namespace controller
       
       if (has_j2_)
       {
-        position_command_[i] = joints_[i][0]->commanded_position_ + joints_[i][1]->commanded_position_;
-      }
+        position_command_[i] = joints_[i][0]->commanded_position_ + joints_[i][1]->commanded_position_;      }
       else
       {
         position_command_[i] = joints_[i][0]->commanded_position_;
       }
-      position_command_[i] = clamp_command(position_command_[i]);  // CHECK!!!!!!
+
+      // position_command_[i] = clamp_command(position_command_[i]);  // CHECK!!!!!!
       
       // Compute position demand from position error:
       error_position = 0.0;
@@ -224,13 +230,13 @@ namespace controller
         error_position = joints_[i][0]->position_ - position_command_[i];
       }
       
-      in_deadband = hysteresis_deadband.is_in_deadband(position_command_[i], error_position, position_deadband);
+      //in_deadband = hysteresis_deadband.is_in_deadband(position_command_[i], error_position, position_deadband);
       
       // don't compute the error if we're in the deadband.
-      if (in_deadband)
-      {
-        error_position = 0.0;
-      }
+      //if (in_deadband)
+      //{
+      //  error_position = 0.0;
+      //}
       
       commanded_effort = pids_[i].computeCommand(-error_position, period);
 
@@ -258,8 +264,7 @@ namespace controller
       }
       
       joints_[i][0]->commanded_effort_ = commanded_effort;
-      
-    }  
+    } 
   }
 
   void SrhGraspController::setCommandCB(const std_msgs::Float64MultiArrayConstPtr &msg)
@@ -267,6 +272,7 @@ namespace controller
     for (int i = 0; i < joints_.size(); ++i)
     {
         joints_[i][0]->commanded_position_ = msg->data[i];
+        ROS_INFO_STREAM("Commanded position: " << joints_[i][0]->commanded_position_);
         if (2 == joints_[i].size())
         {
         joints_[i][1]->commanded_position_ = 0.0;
