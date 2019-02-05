4f7cc992431c2d55e9fdc6864b2cd8e7ada95ac1
ToivoHn <38542384+ToivoHn@users.noreply.github.com>
2019-02-05 16:33:17 +0000

    Update moving_average_filter.py

--- sr_utilities/scripts/moving_average_filter.py
+++ sr_utilities/scripts/moving_average_filter.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+# Copyright (C) 2019 Shadow Robot Company Ltd - All Rights Reserved.
+# Proprietary and Confidential. Unauthorized copying of the content in this file, via any medium is strictly prohibited.
 import rospy
 from std_msgs.msg import Float64
 
