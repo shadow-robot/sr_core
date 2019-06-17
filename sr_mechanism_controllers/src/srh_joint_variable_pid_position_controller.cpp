6943db93b57ac82e085eebb169e2c1bfcfccc26e
Your Name <you@example.com>
2019-06-17 15:56:43 +0000

    license updated

--- sr_mechanism_controllers/src/srh_joint_variable_pid_position_controller.cpp
+++ sr_mechanism_controllers/src/srh_joint_variable_pid_position_controller.cpp
@@ -1,26 +1,28 @@
 /**
  * @file   srh_joint_variable_pid_position_controller.cpp
- * @author Ugo Cupcic <ugo@shadowrobot.com>
+ * @author Anastasios <anastasios@shadowrobot.com>
  * @date   Wed Aug 17 12:32:01 2011
  *
- * Copyright 2011 Shadow Robot Company Ltd.
+ * Copyright 2019 Shadow Robot Company Ltd.
  *
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
- * Software Foundation, either version 2 of the License, or (at your option)
- * any later version.
+ * Software Foundation version 2 of the License.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
  * more details.
  *
  * You should have received a copy of the GNU General Public License along
- * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
  *
  *
  * @brief  Follows a position target. The position demand is converted into a force
  * demand by a PID loop.
+ *
+ * The code is base on srh_joint_position_controller.cpp
  *
  */
 
