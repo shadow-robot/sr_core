9ab618d4f259af237597cd735854f9aee18908b5
mikramarc <michal@shadowrobot.com>
2020-05-04 16:21:03 +0000

    cleanup

--- sr_utilities/test/test_jacobian_utils.cpp
+++ sr_utilities/test/test_jacobian_utils.cpp
@@ -16,9 +16,8 @@ void wait_for_param(std::string param_name, double timeout_in_secs=30.0)
 
 TEST(SrJacobianUtils, test_get_torque_given_wrench)
 {
-  wait_for_param("robot_description");
-  wait_for_param("robot_description_semantic");
-  shadow_robot::SrJacobianUtils ju("robot_description", "rh_first_finger");
+  const int torques_vector_dimension = 4;
+  const std::vector<double> expected_torques{-0.96, 0, 0, 0};
   geometry_msgs::WrenchStamped desired_wrench;
   desired_wrench.wrench.force.x = 10;
   desired_wrench.wrench.force.y = 0;
@@ -27,39 +26,46 @@ TEST(SrJacobianUtils, test_get_torque_given_wrench)
   desired_wrench.wrench.torque.y = 0;
   desired_wrench.wrench.torque.z = 0;
   desired_wrench.header.frame_id = "rh_fftip";
-  Eigen::VectorXd needed_torques = ju.get_torques_given_wrench(desired_wrench);
-  ASSERT_NEAR(needed_torques[0], -0.96, 0.001);
-  ASSERT_NEAR(needed_torques[1], 0, 0.001);
-  ASSERT_NEAR(needed_torques[2], 0, 0.001);
-  ASSERT_NEAR(needed_torques[3], 0, 0.001);
+
+  wait_for_param("robot_description");
+  wait_for_param("robot_description_semantic");
+  
+  shadow_robot::SrJacobianUtils ju("robot_description", "rh_first_finger");
+  Eigen::VectorXd calculated_torques = ju.get_torques_given_wrench(desired_wrench);
+
+  for (int i=0; i<torques_vector_dimension; i++)
+  {
+    ASSERT_NEAR(calculated_torques[i], expected_torques[i], 0.001);
+  }
 }
 
 TEST(SrJacobianUtils, test_get_jacobian)
 {
-  wait_for_param("robot_description");
-  wait_for_param("robot_description_semantic");
-  shadow_robot::SrJacobianUtils ju("robot_description", "rh_first_finger");
-  Eigen::MatrixXd known_jacobian(6,4);
-  known_jacobian << -0.095993, 0, 0, 0,
+  const std::vector<int> jacobian_dimensions{6, 4};
+  Eigen::MatrixXd expected_jacobian(jacobian_dimensions[0], jacobian_dimensions[1]);
+  expected_jacobian << -0.095993, 0, 0, 0,
                     0, -0.0959993, -0.0509995, -0.0259998,
                     0, 0, 0, 0,
                     0, 0.999935, 0.999935, 0.999935,
                     -1, 0, 0, 0,
                     0, -0.0114267, -0.0114267, -0.0114267;
 
-  Eigen::MatrixXd tested_jacobian(6,4);
+  Eigen::MatrixXd tested_jacobian(jacobian_dimensions[0], jacobian_dimensions[1]);
+
+  wait_for_param("robot_description");
+  wait_for_param("robot_description_semantic");
+
+  shadow_robot::SrJacobianUtils ju("robot_description", "rh_first_finger");
   tested_jacobian = ju.get_jacobian();
 
-  for (int i = 0; i < 6; i++)
+  for (int i = 0; i < jacobian_dimensions[0]; i++)
   {
-    for (int j = 0; j < 4; j++)
+    for (int j = 0; j < jacobian_dimensions[1]; j++)
     {
-      ASSERT_NEAR(known_jacobian(i, j), tested_jacobian(i, j), 0.1);
+      ASSERT_NEAR(expected_jacobian(i, j), tested_jacobian(i, j), 0.1);
     }
   }
 }
-
-
 
 int main(int argc, char **argv)
 {
