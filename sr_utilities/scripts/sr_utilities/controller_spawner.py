6cc4f91edeb5fcb67f2e56747f23dca21a15f01e
Ethan Fowler <ethan@shadowrobot.com>
2020-06-23 17:30:33 +0000

    Fixing intermittent bug in controller spawning.

--- sr_utilities/scripts/sr_utilities/controller_spawner.py
+++ sr_utilities/scripts/sr_utilities/controller_spawner.py
@@ -40,6 +40,7 @@ class ControllerSpawner(object):
         self._nonpresent_joints = list(ControllerSpawner.JOINT_NAMES)
         for side in self._joints:
             for joint in self._joints[side]:
+                if joint in self._nonpresent_joints:
                 self._nonpresent_joints.remove(joint)
         if (exclude_wrist):
             self._nonpresent_joints = list(set(self._nonpresent_joints) | set(["rh_WRJ1", "rh_WRJ2",
