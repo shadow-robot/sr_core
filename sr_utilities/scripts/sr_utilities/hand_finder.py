d8e16d306476bf778f3f960282cf91318c5b8e7c
giusebar <giuseppe@shadowrobot.com>
2020-12-16 10:30:12 +0000

    F#src 4643 arm calibration loader (#162)
    * add wait for hand param
    
    * make script wait for joint_state topic
    
    * increase timeout for controller manager
    
    * wait for robot_description
    
    * fix bug
    
    * fix lint
    
    * fix bug

--- sr_utilities/scripts/sr_utilities/hand_finder.py
+++ sr_utilities/scripts/sr_utilities/hand_finder.py
@@ -98,53 +98,55 @@ class HandJoints(object):
         self.joints = {}
         hand_joints = []
         joints = self.get_default_joints()
+        TIMEOUT_WAIT_FOR_PARAMS_IN_SECS = 60.0
+        start_time = rospy.get_time()
+        while not rospy.has_param("/robot_description"):
+            if (rospy.get_time() - start_time > TIMEOUT_WAIT_FOR_PARAMS_IN_SECS):
+                rospy.logwarn("No robot_description found on parameter server."
+                              "Joint names are loaded for 5 finger hand")
+                break
 
-        if rospy.has_param('robot_description'):
-            robot_description = rospy.get_param('robot_description')
+        robot_description = rospy.get_param('robot_description')
 
-            # concatenate all the joints with prefixes
-            for hand in mapping:
-                if hand in joint_prefix:
-                    for joint in joints:
-                        hand_joints.append(joint_prefix[hand] + joint)
-                else:
-                    rospy.logwarn("Cannot find serial " + hand +
-                                  "in joint_prefix parameters")
+        # concatenate all the joints with prefixes
+        for hand in mapping:
+            if hand in joint_prefix:
+                for joint in joints:
+                    hand_joints.append(joint_prefix[hand] + joint)
+            else:
+                rospy.logwarn("Cannot find serial " + hand +
+                              "in joint_prefix parameters")
 
-            # add the prefixed joints to each hand but remove fixed joints
-            hand_urdf = URDF.from_xml_string(robot_description)
-            for hand in mapping:
-                joints_tmp = []
-                self.joints[mapping[hand]] = []
-                for joint in hand_urdf.joints:
-                    if joint.type != 'fixed':
-                        prefix = joint.name[:3]
-                        # is there an empty prefix ?
-                        if "" in joint_prefix.values():
-                            joints_tmp.append(joint.name)
-                        elif prefix not in joint_prefix.values():
-                            rospy.logdebug("joint " + joint.name + " has invalid "
-                                           "prefix:" + prefix)
-                        elif prefix == joint_prefix[hand]:
-                            joints_tmp.append(joint.name)
-                for joint_unordered in hand_joints:
-                    if joint_unordered in joints_tmp:
-                        self.joints[mapping[hand]].append(joint_unordered)
+        # add the prefixed joints to each hand but remove fixed joints
+        hand_urdf = URDF.from_xml_string(robot_description)
+        for hand in mapping:
+            joints_tmp = []
+            self.joints[mapping[hand]] = []
+            for joint in hand_urdf.joints:
+                if joint.type != 'fixed':
+                    prefix = joint.name[:3]
+                    # is there an empty prefix ?
+                    if "" in joint_prefix.values():
+                        joints_tmp.append(joint.name)
+                    elif prefix not in joint_prefix.values():
+                        rospy.logdebug("joint " + joint.name + " has invalid "
+                                       "prefix:" + prefix)
+                    elif prefix == joint_prefix[hand]:
+                        joints_tmp.append(joint.name)
+            for joint_unordered in hand_joints:
+                if joint_unordered in joints_tmp:
+                    self.joints[mapping[hand]].append(joint_unordered)
 
-        else:
-            rospy.logwarn("No robot_description found on parameter server."
-                          "Joint names are loaded for 5 finger hand")
-
-            # concatenate all the joints with prefixes
-            for hand in mapping:
-                hand_joints = []
-                if hand in joint_prefix:
-                    for joint in joints:
-                        hand_joints.append(joint_prefix[hand] + joint)
-                else:
-                    rospy.logwarn("Cannot find serial " + hand +
-                                  "in joint_prefix parameters")
-                self.joints[mapping[hand]] = hand_joints
+        # concatenate all the joints with prefixes
+        for hand in mapping:
+            hand_joints = []
+            if hand in joint_prefix:
+                for joint in joints:
+                    hand_joints.append(joint_prefix[hand] + joint)
+            else:
+                rospy.logwarn("Cannot find serial " + hand +
+                              "in joint_prefix parameters")
+            self.joints[mapping[hand]] = hand_joints
 
 
 class HandFinder(object):
@@ -158,30 +160,34 @@ class HandFinder(object):
         """
         Parses the parameter server to extract the necessary information.
         """
+        self._hand_e = False
+        self._hand_parameters = {'joint_prefix': {}, 'mapping': {}}
+        self._hand_h = False
+        self._hand_h_parameters = {}
 
-        hand_parameters = {}
-        if rospy.has_param("/hand"):
-            self._hand_e = True
-            hand_parameters = rospy.get_param("/hand")
-        else:
-            self._hand_e = False
-            hand_parameters = {'joint_prefix': {}, 'mapping': {}}
+        TIMEOUT_WAIT_FOR_PARAMS_IN_SECS = 60.0
+        self.wait_for_hand_params(TIMEOUT_WAIT_FOR_PARAMS_IN_SECS)
 
-        if rospy.has_param("/fh_hand"):
-            self._hand_h = True
-            self._hand_h_parameters = rospy.get_param("/fh_hand")
-        else:
-            self._hand_h = False
-            self._hand_h_parameters = {}
-
-        if not (self._hand_e or self._hand_h):
-            rospy.logerr("No hand is detected")
-
-        self.hand_config = HandConfig(hand_parameters["mapping"],
-                                      hand_parameters["joint_prefix"])
+        self.hand_config = HandConfig(self._hand_parameters["mapping"],
+                                      self._hand_parameters["joint_prefix"])
         self.hand_joints = HandJoints(self.hand_config.mapping, self.hand_config.joint_prefix).joints
         self.calibration_path = HandCalibration(self.hand_config.mapping).calibration_path
         self.hand_control_tuning = HandControllerTuning(self.hand_config.mapping)
+
+    def wait_for_hand_params(self, timeout_in_secs):
+        start_time = rospy.get_time()
+        while not rospy.has_param("/hand") and not rospy.has_param("/fh_hand"):
+            if (rospy.get_time() - start_time > timeout_in_secs):
+                rospy.logerr("No hand is detected")
+                break
+        if rospy.has_param("/hand"):
+            rospy.loginfo("Found hand E")
+            self._hand_e = True
+            self._hand_parameters = rospy.get_param("/hand")
+        elif rospy.has_param("/fh_hand"):
+            rospy.loginfo("Found hand H")
+            self._hand_h = True
+            self._hand_h_parameters = rospy.get_param("/fh_hand")
 
     def get_calibration_path(self):
         if not self._hand_e:
