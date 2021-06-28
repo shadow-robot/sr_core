9f7c364b9f6b50b31b26123c4c4110dd885745ac
Fotios Papadopoulos <fotios@shadowrobot.com>
2021-06-28 16:29:39 +0100

    Update simple_transmission.hpp

--- sr_mechanism_model/include/sr_mechanism_model/simple_transmission.hpp
+++ sr_mechanism_model/include/sr_mechanism_model/simple_transmission.hpp
@@ -55,7 +55,7 @@ public:
   ~SimpleTransmission()
   {
     ROS_DEBUG_STREAM("Destroying simple transmission");
-  }  // added degub stream line because empty destructor make sr-ros-interface-interface unit test fail
+  }  // added degub stream line because empty destructor make sr-ros-interface-interface unit test fail blblblblblblblblbl
 
   bool initXml(tinyxml2::XMLElement *config, ros_ethercat_model::RobotState *robot);
 
