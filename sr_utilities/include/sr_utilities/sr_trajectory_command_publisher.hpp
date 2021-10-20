e4da6a3b526a2e051fe6860231db8ef1f7f7a7c9
mykolasjuraitis <75975027+mykolasjuraitis@users.noreply.github.com>
2021-10-20 13:30:22 +0000

    SRC-6656 Create trajectory command publisher utility class (#197)
    * SRC-6656 Create trajectory command publisher utility class
    
    * Include what you use
    
    * 2011 -> 2021
    
    * Create publishers on demand
    
    * Add trailing underscore
    
    * Throw exception instead of ignoring joint without publisher
    
    * Cache trajectory messages together with publishers
    
    * Add comment for clarity
    
    * Add #include <utility> for pair<>

---
+++ sr_utilities/include/sr_utilities/sr_trajectory_command_publisher.hpp
@@ -0,0 +1,38 @@
+/*
+* Copyright 2021 Shadow Robot Company Ltd.
+*
+* This program is free software: you can redistribute it and/or modify it
+* under the terms of the GNU General Public License as published by the Free
+* Software Foundation version 2 of the License.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+* more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _SR_TRAJECTORY_COMMAND_PUBLISHER_HPP_
+#define _SR_TRAJECTORY_COMMAND_PUBLISHER_HPP_
+
+#include <map>
+#include <memory>
+#include <ros/ros.h>
+#include <string>
+#include <trajectory_msgs/JointTrajectory.h>
+#include <utility>
+
+class SrTrajectoryCommandPublisher
+{
+public:
+  void publish(trajectory_msgs::JointTrajectory joint_trajectory);
+private:
+  std::map<std::string, std::shared_ptr<std::pair<ros::Publisher,
+    trajectory_msgs::JointTrajectory>>> joint_to_publisher_and_msg_;
+  std::shared_ptr<std::pair<ros::Publisher, trajectory_msgs::JointTrajectory>>
+    get_publisher_and_msg(std::string joint_name);
+};
+
+#endif  // _SR_TRAJECTORY_COMMAND_PUBLISHER_HPP_
