6e0c8180d4e5bbfe75552e02f76061b5e31a4cfa
Robert Haschke <rhaschke@users.noreply.github.com>
2021-12-10 09:55:53 +0100

    setup.py: Use setuptools instead of distutils (#202)
    See http://wiki.ros.org/noetic/Migration#Setuptools_instead_of_Distutils

--- sr_tactile_sensors/setup.py
+++ sr_tactile_sensors/setup.py
@@ -1,5 +1,5 @@
 from __future__ import absolute_import
-from distutils.core import setup
+from setuptools import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
