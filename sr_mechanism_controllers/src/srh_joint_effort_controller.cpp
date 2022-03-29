5ceab64b8baf7ce8886b79ff8d50d0fe8624a677
georgiablanco <georgia@shadowrobot.com>
2022-03-29 12:42:06 +0000

    changing read params function to correct place

--- sr_mechanism_controllers/src/srh_joint_effort_controller.cpp
+++ sr_mechanism_controllers/src/srh_joint_effort_controller.cpp
@@ -114,6 +114,8 @@ namespace controller
     ROS_DEBUG_STREAM(" In Init: " << getJointName() << " This: " << this
                      << " joint_state: " << joint_state_);
 
+    read_parameters();
+
     after_init();
     return true;
   }
@@ -121,7 +123,6 @@ namespace controller
   void SrhEffortJointController::starting(const ros::Time &time)
   {
     command_ = 0.0;
-    read_parameters();
   }
 
   bool SrhEffortJointController::setGains(sr_robot_msgs::SetEffortControllerGains::Request &req,
